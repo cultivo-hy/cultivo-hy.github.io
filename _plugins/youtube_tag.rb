dfce8edcb5d2235d509dac6d217a8d205fbb1b65
HoYong LEE <dwhylee@gmail.com>
2019-01-17 21:31:03 +0900

    Update youtube_tag.rb

--- _plugins/youtube_tag.rb
+++ _plugins/youtube_tag.rb
@@ -1,8 +1,8 @@
-# Title: Responsive Lazy Load YouTube embed tag for Jekyll
+# Title: Responsive YouTube embed tag for Jekyll
 # Author: Brett Terpstra <http://brettterpstra.com>
-# Description: Output a styled element for onClick replacement with responsive layout
+# Description: Output a simple YouTube embed tag with code to make it responsive
 #
-# Syntax {% youtube video_id [width height] ["Caption"] %}
+# Syntax {% youtube video_id [width height] %}
 #
 # Example:
 # {% youtube B4g4zTF5lDo 480 360 %}
@@ -15,23 +15,23 @@ module Jekyll
     @height = ''
 
     def initialize(tag_name, markup, tokens)
-      if markup =~ /(?:(?:https?:\/\/)?(?:www.youtube.com\/(?:embed\/|watch\?v=)|youtu.be\/)?(\S+)(?:\?rel=\d)?)(?:\s+(\d+)\s(\d+))?(?:\s+"(.*?)")?/i
+      if markup =~ /(?:(?:https?:\/\/)?(?:www.youtube.com\/(?:embed\/|watch\?v=)|youtu.be\/)?(\S+)(?:\?rel=\d)?)(?:\s+(\d+)\s(\d+))?/i
         @videoid = $1
-        @width = $2 || "480"
-        @height = $3 || "360"
-        @caption = $4 ? "<figcaption>#{$4}</figcaption>" : ""
+        @width = $2 || "640"
+        @height = $3 || "480"
       end
       super
     end
 
     def render(context)
-      context.environments.first['page']['youtube'] = @videoid
+      ouptut = super
       if @videoid
         # Thanks to Andrew Clark for the inline CSS calculation idea <http://contentioninvain.com/2013/02/13/video-embeds-for-responsive-designs/>
         intrinsic = ((@height.to_f / @width.to_f) * 100)
         padding_bottom = ("%.2f" % intrinsic).to_s  + "%"
-        video = %Q{<a class="youtube" href="https://www.youtube.com/watch?v=#{@videoid}" data-videoid="#{@videoid}" data-width="#{@width}" data-height="#{@height}">YouTube Video</a>}
-        %Q{<figure class="bt-video-container" style="padding-bottom:#{padding_bottom}">#{video}#{@caption}</figure>}
+        # remove/comment the next line and adjust the class name on the following line if you already have CSS for responsive video
+        video = "<style>.bt-video-container iframe,.bt-video-container object,.bt-video-container embed{position:absolute;top:0;left:0;width:100%;height:100%;margin-top:0}</style>\n"
+        video += %Q{<div class="bt-video-container" style="position:relative;padding-bottom:56.25%;height:auto;max-width:100%overflow:hidden"><iframe width="#{@width}" height="#{@height}" src="http://www.youtube.com/embed/#{@videoid}?rel=0" frameborder="0" allowfullscreen></iframe></div>}
       else
         "Error processing input, expected syntax: {% youtube video_id [width height] %}"
       end
